2487dd2c77fe66d7a8400adf4bbfe24e05f1f8e7
Rafael Khan <raf.khan.f@gmail.com>
2014-12-12 04:12:34 -0500

    Initial commit

---
+++ darude.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var lame = require('lame');
+var Speaker = require('speaker');
+
+var mp3Stream = fs.createReadStream('sandstorm.mp3');
+
+mp3Stream.pipe(new lame.Decoder)
+  .on('format', console.log)
+  .pipe(new Speaker);
