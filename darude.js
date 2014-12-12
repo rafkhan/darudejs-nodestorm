df7047bc58472ba5717ad6b8df043bf82f6dd588
Rafael Khan <raf.khan.f@gmail.com>
2014-12-12 04:29:24 -0500

    Path resolves correctly now

--- darude.js
+++ darude.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
+var path = require('path');
 var lame = require('lame');
 var Speaker = require('speaker');
 
-var mp3Stream = fs.createReadStream(__dirname + 'sandstorm.mp3');
+var fpath = path.resolve(__dirname, 'sandstorm.mp3');
+console.log(fpath);
+var mp3Stream = fs.createReadStream(fpath);
 
 mp3Stream.pipe(new lame.Decoder)
   .on('format', console.log)
