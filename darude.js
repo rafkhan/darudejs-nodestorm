3856e94a0d3ecd7b629f667bfeaec7d9898da88a
Rafael Khan <raf.khan.f@gmail.com>
2014-12-12 04:18:56 -0500

    Path relative to __dirname

--- darude.js
+++ darude.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var lame = require('lame');
 var Speaker = require('speaker');
 
-var mp3Stream = fs.createReadStream('sandstorm.mp3');
+var mp3Stream = fs.createReadStream(__dirname + 'sandstorm.mp3');
 
 mp3Stream.pipe(new lame.Decoder)
   .on('format', console.log)
